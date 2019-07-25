59bc24c4c7d0b0170a209fba66fa943cf6b60996
Manuela Rommel <Manu1@Manuelas-MacBook-Pro.local>
2019-07-25 19:43:24 +0200

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import NavbarI from "./components/NavbarI";
 import Index from "./containers/Index";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Index} />
+          <Route exact path="/auth/signup" component={Signup} />
+          <Route exact path="/auth/login" component={Login} />}
         </Switch>
       </BrowserRouter>
     </div>
---
+++ client/src/components/Login.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import login from "../services/api";
+import { Form, Button, Alert } from "react-bootstrap";
+
+export default class Login extends Component {
+  state = {
+    username: "",
+    password: "",
+    email: "",
+    error: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    const { username, password, email } = this.state;
+
+    event.preventDefault();
+
+    login(username, password, email)
+      .then(data => {
+        this.props.setUser(data);
+        this.props.push("/login");
+      })
+      .catch(err => {
+        this.setState({ error: err.response.data.message });
+      });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+          <Form.Label htmlFor="username">Username:</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            id="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="password">Password:</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            id="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="email">Email:</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            id="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+
+        {this.state.error && (
+          <Alert variant="warning">{this.state.error}</Alert>
+        )}
+
+        <Button type="submit">Login</Button>
+      </Form>
+    );
+  }
+}
