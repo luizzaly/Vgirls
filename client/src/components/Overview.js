944f8c0cca38e053a5da45faebcc9fb1de355a5f
Luisa Schrott <luisa.schrott@gmail.com>
2019-07-26 14:42:29 +0200

    Overview

--- client/src/components/Overview.js
+++ client/src/components/Overview.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Overview extends Component {
   state = {
-    bodparts: [],
+    vulva: [],
     title: ""
   };
+
+  getData = () => {
+    axios
+      .get("/vulva")
+      .then(response => {
+        this.setState({
+          vulva: response.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.getData();
+  }
+
   render() {
+    let bodypart = this.state.vulva.find(part => {
+      return part.title === this.props.match.params.title;
+    });
+    console.log("render");
+
     return (
       <div>
-        {this.props.location.state.title}
-        fghjkl;';lkjhgf'
+        {/* {this.props.location.state.title} */}
+        {bodypart && bodypart.title}
         <p>Ethynology (how it all starts)</p>
         <p>Description (how it all works)</p>
         <p>More Info (all you need to know)</p>
--- client/src/containers/Vulva.js
+++ client/src/containers/Vulva.js
@@ -39,38 +39,95 @@ export default class Vulva extends Component {
     // let bodypart = this.state.vulva.find(part => {
     //   return part.title === this.state.selected;
     // });
-    // console.log(bodypart);
 
     return (
       <div>
         {/* on 1st render, this.state.projects is [] */}
         {/* after that, this.state.projects is populated by the data from the API */}
-        (
-        <img
-          onClick={this.handleclick}
-          src="/img/Mumu.svg"
-          alt="Vulva orange"
-        />
+
         <div
           className="image-container"
-          style={{ width: "100vw", position: "relative" }}
+          style={{ width: "414px", position: "relative", margin: " 0 auto" }}
         >
-          <Link to="/outer">
+          <Link to="/vulva/Labia majora pudendi">
             <img
-              src="/img/Asset3.png"
+              src="/img/basis.png"
               alt=""
-              style={{ position: "absolute", top: 0, left: 0 }}
+              style={{ position: "absolute", top: 0, left: "20%" }}
             />
           </Link>
-          <Link to="/inner">
+          <Link to="/vulva/Labia minora pudendi">
             <img
-              src="/img/Asset4.png"
+              src="/img/Komissur.png"
               alt=""
-              style={{ position: "absolute", top: 0, left: 0 }}
+              style={{ position: "absolute", top: 0, left: "27%" }}
+            />
+          </Link>
+          <Link to="/vulva/Vestibulum vaginae">
+            <img
+              src="/img/Scheide.png"
+              alt=""
+              style={{ position: "absolute", top: "50px", left: "38%" }}
+            />
+          </Link>
+          <Link to="/vulva/inner">
+            <img
+              src="/img/Introitusvaginae.png"
+              alt=""
+              style={{ position: "absolute", top: "134px", left: "47%" }}
+            />
+          </Link>
+
+          <Link to="/vulva">
+            <img
+              src="/img/smallleft.png"
+              alt=""
+              style={{ position: "absolute", top: "176px", left: "60%" }}
+            />
+          </Link>
+          <Link to="/vulva">
+            <img
+              src="/img/smalltop.png"
+              alt=""
+              style={{ position: "absolute", top: "19px", left: "59.5%" }}
+            />
+          </Link>
+          <Link to="/vulva">
+            <img
+              src="/img/smalltopleft.png"
+              alt=""
+              style={{ position: "absolute", top: "20px", left: "34%" }}
+            />
+          </Link>
+          <Link to="/vulva/Clitoris">
+            <img
+              src="/img/Clitoris.png"
+              alt=""
+              style={{ position: "absolute", top: "8px", left: "49.5%" }}
+            />
+          </Link>
+          <Link to="/vulva">
+            <img
+              src="/img/urethra.png"
+              alt=""
+              style={{ position: "absolute", top: "87px", left: "51.5%" }}
+            />
+          </Link>
+          <Link to="/vulva">
+            <img
+              src="/img/right.png"
+              alt=""
+              style={{ position: "absolute", top: "172px", left: "28%" }}
+            />
+          </Link>
+          <Link to="/vulva/Hymen">
+            <img
+              src="/img/hymen.png"
+              alt=""
+              style={{ position: "absolute", top: "250px", left: "50%" }}
             />
           </Link>
         </div>
-        )
       </div>
     );
   }
