28ce63bfd0ad3f6f4ac6441be84c76a5efc2b966
Luisa Schrott <luisa.schrott@gmail.com>
2019-08-02 00:34:44 +0200

    pictures

--- client/build/precache-manifest.a11219ba1237a126a42feca336043cfc.js
+++ client/build/precache-manifest.a11219ba1237a126a42feca336043cfc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f01de7b1e3386cfeefd1ddb973cc5e29",
+    "revision": "3e992b1ccf681cd6f799049c331cdae7",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "3c7b52a481f04358858f",
+    "revision": "b24577051857d17ab48c",
     "url": "/static/css/main.de45cf9e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.daa16d5f.chunk.js"
   },
   {
-    "revision": "3c7b52a481f04358858f",
-    "url": "/static/js/main.25d6c750.chunk.js"
+    "revision": "b24577051857d17ab48c",
+    "url": "/static/js/main.5d248a06.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
