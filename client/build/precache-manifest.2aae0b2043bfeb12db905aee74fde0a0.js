8200e2376d741bbcfc27fe00f87d40e8eae77acd
Luisa Schrott <luisa.schrott@gmail.com>
2019-08-01 23:03:14 +0200

    heroku

---
+++ client/build/precache-manifest.2aae0b2043bfeb12db905aee74fde0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390b4abdca5bee128142b20770ec6400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a206d2851955c2b3f22",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "19e35597c7ba0f4d8b1d",
+    "url": "/static/css/main.daf002a8.chunk.css"
+  },
+  {
+    "revision": "7a206d2851955c2b3f22",
+    "url": "/static/js/2.daa16d5f.chunk.js"
+  },
+  {
+    "revision": "19e35597c7ba0f4d8b1d",
+    "url": "/static/js/main.695f6070.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
