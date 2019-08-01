1362c408357ef02d118664275da5c97e7658e949
maelaro <manuelasakurarommel@yahoo.de>
2019-08-01 19:52:57 +0200

    landing page

--- client/src/containers/Index.js
+++ client/src/containers/Index.js
@@ -4,66 +4,74 @@ import Carousel from "react-bootstrap/Carousel";
 const Home = () => {
   return (
     <div>
-      <div class="header-container" />
-      <h1 class="valueprop">
-        No Tabus here!
-        <br /> Let's discover the female genitales
-      </h1>
-      <div>
-        <h2 class="header-text">We are all different</h2>
+      <div class="header-container">
+        <h1 class="valueprop">Empower yourself</h1>
+        <br />
+        <h1 class="valuepropo">Let's discover the female body</h1>
+
+        <h3 class="header-text">We are all different</h3>
       </div>
-      <div>
+      <div className="carouselcolor">
         <Carousel>
           <Carousel.Item>
             <img
               className="d-block w-100"
               src="/img/vagina1.png"
               alt="First slide"
+              width={200}
+              height={400}
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
               src="/img/vagina2.png"
               alt="Third slide"
+              width={200}
+              height={400}
             />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
               src="/img/vagina3.png"
               alt="Third slide"
+              width={200}
+              height={400}
             />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
               src="/img/vagina4.png"
               alt="First slide"
+              width={200}
+              height={400}
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>
+
+      <div className="dreieck" />
+      <div className="about">
+        <h3>About</h3>
+        <p>
+          The female body. So many myths, so many secrets. We from VGirls want
+          to educate and empower young women with knowledge about their bodies.
+          Click on the bodypart you want to know more about.
+        </p>
+      </div>
+      {/* <svg
+        id="bottom-triangle"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        width="100%"
+        height="100"
+        viewBox="0 0 100 102"
+        preserveAspectRatio="none"
+      >
+        <path d="M0 0 L50 100 L100 0 Z" />
+      </svg> */}
     </div>
   );
 };
